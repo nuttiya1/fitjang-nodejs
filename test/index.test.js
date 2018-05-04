7e3b433a10543bd0fe20d7ebd4a2c8850992546a
nuttiya1 <36584175+nuttiya1@users.noreply.github.com>
2018-05-05 01:25:02 +0700

    Update index.test.js

--- test/index.test.js
+++ test/index.test.js
@@ -8,41 +8,16 @@ const supertest = require('supertest');
 const should  = require('should');
 const chai = require('chai');
 
+var path = require("path");
+var models = require(path.join(__dirname, "../"));
 
 var server = supertest.agent("http://localhost:3000");
 
-describe("Sample unit test", function(){
-  it("should return 404", function(done){
-    server
-    .get("/random")
-    .expect(404)
-    .end(function(err,res){
-      res.status.should.equal(404);
-      done();
-    });
-  });
-
-  it('Url homepage', function(done) {
-    request.get({url : '/'},
-      function(error, response){
-          expect(200);
-          done();
-      });
-    });
-
-    it('Url show', function(done) {
-      request.get({url : '/show'},
-        function(error, response){
-            expect(200);
-            done();
-        });
-      });
-
+describe("Test about add and delete data", function(){
   it("should return home page", function(done){
     server
     .post('/')
-    .send({activity : "Run", weight : "50", time : "110"})
-    .expect("Content-type",/json/)
+    .send({activity : "Run", weight : "50", time : "50"})
     .expect(200)
     .end(function(err, res){
       should(res.status).equal(200);
@@ -59,5 +34,48 @@ describe("Sample unit test", function(){
       should(res.status).equal(200);
       done();
       });
+    });
+  it("should return new data", function(done){
+    server
+    .get('/test')
+    .expect(200)
+    .end(function(err, res){
+      should(res.status).equal(200);
+      console.log(res.text);
+      done();
+    });
+  });
+  it("should delete data", function(done, request){
+    server
+    .post('/deleteTable')
+    .send({id : "331"})
+    .expect(200)
+    .end(function(err, res){
+      done();
+    });
+  });
+});
+
+describe('GET url', function(){
+  it("url homepage", function(done) {
+    request.get({url : '/'},
+      function(error, response, body){
+          expect(200);
+          done();
+      });
+    });
+  it("url show", function(done) {
+    request.get({url : '/show'},
+      function(error, response, body){
+          expect(200);
+          done();
+      });
+    });
+  it("url test", function(done) {
+      request.get({url : '/test'},
+        function(error, response, body){
+            expect(200);
+            done();
+        });
     });
 });
